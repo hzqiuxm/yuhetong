61c5d8cd85808b9887d16398ecffdaafda74bba1
seanwu <seanwu@yunhetong.net>
2014-08-06 17:12:55 +0800

    initFlask

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#!/yunhetong/
+__author__ = 'Seanwu'
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/test/<path:filename>')
+def template_load(filename=None):
+    if not filename:
+        return render_template('index.html')
+    else:
+        return render_template(filename+'.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
