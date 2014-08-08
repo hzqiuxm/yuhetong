09eba6dd599899f83f8ec86f7561d7ee7ae9637f
seanwu <seanwu@yunhetong.net>
2014-08-08 17:08:40 +0800

    test

--- yunapp/test/views.py
+++ yunapp/test/views.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, render_template
+
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/home/seanwu/uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 test = Blueprint('test', __name__)
+test.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @test.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @test.route('/<path:filename>')
