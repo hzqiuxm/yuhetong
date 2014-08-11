9a04d00f6d2c43c57bd2b5ff5791a617bcb72dd4
wenwu <xudabin@yunhetong.com>
2014-08-11 14:52:34 +0800

    add user blue print

--- yunapp/__init__.py
+++ yunapp/__init__.py
@@ -3,8 +3,10 @@
 
 from flask import Flask
 from test.views import test as mod_test
+from user.views import user as mod_user
 app = Flask('yunhetong')
 
 app.register_blueprint(mod_test, url_prefix='/test')
+app.register_blueprint(mod_user, url_prefix='/user')
 
 
--- yunapp/user/views.py
+++ yunapp/user/views.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template
-user = Blueprint('user', __name__, url_prefix='/user')
+from flask import Blueprint, render_template, jsonify
+user = Blueprint('user', __name__)
 
-@user.route('/')
-def index():
-    render_template('/user/user_index.html')
+@user.route('/register', methods=['POST'])
+def register():
+
+    return_dict = {'success': True, 'uid':'uid'}
+    return jsonify(return_dict)
+
+@user.route('/namecheck')
+def namecheck():
+    return_dict = {'success': True, 'errorMsg':''}
+    return jsonify(return_dict)
+
+@user.route('/login')
+def login():
+    return_dict = {'success': True, 'errorMsg':''}
+    return jsonify(return_dict)
