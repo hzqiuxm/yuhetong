96c537492a442db049de5f11f3063403ef4b1eba
seanwu <seanwu@yunhetong.net>
2014-08-11 17:31:32 +0800

    user model
    Summary: xiugai l user d model wenjian
    
    Test Plan: ...
    
    Reviewers: wenwu
    
    Subscribers: seanwu
    
    Differential Revision: http://phabricator.lx.com/D6

--- yunapp/user/views.py
+++ yunapp/user/views.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, render_template, jsonify
+import flask.ext.login.LoginManager as LoginManager
+from models import User
+login_manager = LoginManager()
+
 user = Blueprint('user', __name__)
+login_manager.init_app(user)
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid)
 
 @user.route('/register', methods=['POST'])
 def register():
@@ -9,10 +19,12 @@ def register():
     return_dict = {'success': True, 'uid':'uid'}
     return jsonify(return_dict)
 
+
 @user.route('/namecheck')
 def namecheck():
     return_dict = {'success': True, 'errorMsg':''}
     return jsonify(return_dict)
+
 
 @user.route('/login')
 def login():
