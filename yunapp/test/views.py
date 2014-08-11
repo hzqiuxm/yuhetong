edd6d9d44bbe1dc0f17449f7bcb9ae9614c7281c
seanwu <seanwu@yunhetong.net>
2014-08-11 14:17:38 +0800

    file upload simple demo

--- yunapp/test/views.py
+++ yunapp/test/views.py
@@ -4,23 +4,27 @@ from flask import Blueprint, render_template
 
 import os
 from flask import Flask, request, redirect, url_for
-#from werkzeug import secure_filename
 
-# UPLOAD_FOLDER = '/home/seanwu/uploads/'
-# ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-#
-# test.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 test = Blueprint('test', __name__)
 
+UPLOAD_FOLDER = '/home/seanwu/uploads/'
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @test.route('/')
 def hello_world():
     return render_template('index.html')
 
-@test.route('/file/upload', methods=['GET', 'POST'])
+@test.route('/file/upload', methods=['POST'])
 def file_upload():
-    return '{"fid":"000"}'
+    if request.method == 'POST':
+        file = request.files['userfile']
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+    return '{"fid":"000","fname":'+filename+'}'
 
 @test.route('/<path:filename>')
 def template_load(filename=None):
@@ -29,3 +33,7 @@ def template_load(filename=None):
     else:
         return render_template(filename+'.html')
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
