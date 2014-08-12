1fb5b8a6b98042c7b80edf07f2a069c09892f738
seanwu <seanwu@yunhetong.net>
2014-08-12 20:15:02 +0800

    fix some mistake

--- yunapp/orm/db.py
+++ yunapp/orm/db.py
@@ -1,4 +1,4 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # __author__ = 'micheal'
 
 from sqlalchemy import MetaData, Table, Column, ForeignKey
@@ -11,31 +11,30 @@ _metadata = None
 if not _metadata:
     _metadata = MetaData()
 
-
 t_lxuser = Table('lxuser', _metadata,
-    Column('id', INTEGER, primary_key=True,autoincrement=True),
-    Column('type', TINYINT(10),unique=True),
-    Column('userName', VARCHAR(64), unique=True),
-    Column('realName', VARCHAR(64), unique=True),
-    Column('passwd', VARCHAR(64), unique=True),
-    Column('email', VARCHAR(64), unique=True),
-    Column('phone', VARCHAR(32)),
-    Column('parentUserId', INTEGER, unique=True),
-    Column('companyId', INTEGER, unique=True),
-    Column('signId', INTEGER, ),
-    Column('status', TINYINT, ),
-    Column('createTime', TIMESTAMP, unique=True,default='0000-00-00 00:00:00'),
-    Column('modifyTime', TIMESTAMP, unique=True,default=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))),
-    mysql_engine='InnoDB',
-)
+                 Column('id', INTEGER, primary_key=True, autoincrement=True),
+                 Column('type', TINYINT(10), nullable=False, nullable=False),
+                 Column('userName', VARCHAR(64), nullable=False),
+                 Column('realName', VARCHAR(64), nullable=False),
+                 Column('passwd', VARCHAR(64), nullable=False),
+                 Column('email', VARCHAR(64), nullable=False),
+                 Column('phone', VARCHAR(32)),
+                 Column('parentUserId', INTEGER, nullable=False),
+                 Column('companyId', INTEGER, nullable=False),
+                 Column('signId', INTEGER, ),
+                 Column('status', TINYINT, ),
+                 Column('createTime', TIMESTAMP, nullable=False, default='0000-00-00 00:00:00'),
+                 Column('modifyTime', TIMESTAMP, nullable=False,
+                        default=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))),
+                 mysql_engine='InnoDB',)
 
 t_lxfile = Table('lxfile', _metadata,
-    Column('id', INTEGER, primary_key=True),
-    Column('fuuid', VARCHAR(32)),
-    Column('type', TINYINT),
-    Column('name', VARCHAR(64)),
-    Column('status', TINYINT),
-    Column('createTime', TIMESTAMP),
-    Column('modifyTime', TIMESTAMP),
-    mysql_engine='InnoDB',
+                 Column('id', INTEGER, primary_key=True),
+                 Column('fuuid', VARCHAR(32)),
+                 Column('type', TINYINT),
+                 Column('name', VARCHAR(64)),
+                 Column('status', TINYINT),
+                 Column('createTime', TIMESTAMP),
+                 Column('modifyTime', TIMESTAMP),
+                 mysql_engine='InnoDB',
 )
