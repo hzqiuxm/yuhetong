c372ec5f0277c508d4df0389e8e11a66555ac314
micheal <xudb@shterm.com>
2014-08-12 11:10:36 +0800

    remove ignore

---
+++ yunapp/config.py
@@ -0,0 +1,18 @@
+DEBUG = False
+SECRET_KEY = 'mysecretkey'
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+DATABASE_URI = 'mysql://root:Hello1234@127.0.0.1/wenwu_test'
+
+
+DB_NAME = 'wenwu_test'
+DB_USER = 'root'
+DB_HOST = '127.0.0.1'
+DB_PASSWD = 'Hello1234'
+
+def get_db_config():
+    db_config = {}
+    db_config['db_name'] = DB_NAME
+    db_config['db_user'] = DB_USER
+    db_config['db_host'] = DB_HOST
+    db_config['db_password'] = DB_PASSWD
+    return db_config
