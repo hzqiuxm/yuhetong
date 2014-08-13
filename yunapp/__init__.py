9f102344af32e7877198617a5e5ac0f6fbf00d5a
micheal <xudb@shterm.com>
2014-08-14 00:22:11 +0800

    add

--- yunapp/__init__.py
+++ yunapp/__init__.py
@@ -6,6 +6,7 @@ from test.views import test as mod_test
 from user.views import user as mod_user
 from file.views import mod_file
 from yunapps import app
+from yunapp import utils
 import jinja2
 my_loader = jinja2.ChoiceLoader([
     app.jinja_loader,
@@ -20,4 +21,6 @@ app.register_blueprint(mod_test, url_prefix='/test')
 app.register_blueprint(mod_user, url_prefix='/user')
 app.register_blueprint(mod_file, url_prefix='/file')
 
-
+@app.route("/sitemap.html", methods=['GET'])
+def site_map():
+    return utils.show_site_map(app.url_map.iter_rules())
--- yunapp/utils.py
+++ yunapp/utils.py
@@ -1,3 +1,26 @@
 # -*- coding: utf-8 -*-
 ## Common function  or comman constans
+import cgi
 
+def show_site_map(rules, prefix=None):
+    links = []
+    support_method = set(['GET', 'POST', 'PUT', 'DELETE'])
+
+    for rule in rules:
+        method = support_method & rule.methods
+        links.append((rule.rule, method.pop(), rule.endpoint))
+
+    s = []
+    s.append('<h2>Site Map</h2><table width="800"><tr><td width="20%"><b>Method</b>')
+    s.append('</td><td><b>URI</b></td><td><b>Function</b></td></tr>')
+
+    links = sorted(links, key=lambda link: (link[0], link[1]))
+    if prefix is None:
+        prefix = ''
+    for url, method, name in links:
+        print prefix+url
+        s.append('<tr><td>[%s]</td><td><a href="%s" target="_blank">%s</a>'\
+            '</td><td>%s</td></tr>' % (method, prefix+url, cgi.escape(url), name))
+
+    s.append('</table>')
+    return ''.join(s)
