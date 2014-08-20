aedf51d1b25d1174644c976fafef49b0036ef060
wenwu <xudabin@yunhetong.com>
2014-08-20 15:12:54 +0800

    add template test

--- yuntest/template/template_tests.py
+++ yuntest/template/template_tests.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, json
 from yunapp import app
 
 class TestTemplate(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestTemplate(unittest.TestCase):
         pass
 
     def add_template(self):
-        return self.app.post('/ctemplate/templates', data=dict(
+        rv = self.app.post('/ctemplate/templates', data=dict(
             template_name='test_template',
             template_type_id = 4,
             template_content = """
@@ -20,29 +20,108 @@ class TestTemplate(unittest.TestCase):
             """
         ), follow_redirects=True)
 
-    def del_template(self, template_id):
-        return self.app.delete('/ctemplate/' + template_id,
-                               follow_redirects=True)
+        if rv.status_code != 200:
+            print 'add template status_code error' + rv.status_code
+            return None
+        return json.loads(rv.data)
 
+    def del_template(self, template_id):
+        rv = self.app.delete('/ctemplate/' + str(template_id), follow_redirects=True)
+        if rv.status_code != 200:
+            print 'delete template status_code error' + rv.status_code
+            return None
+        return json.loads(rv.data)
+
+    def update_template(self, template_id):
+        rv = self.app.put('/ctemplate/' + str(template_id),data=dict(
+            template_name = 'test_template_updated'
+        ), follow_redirects=True)
+        if rv.status_code != 200:
+            print 'delete template status_code error' + rv.status_code
+            return None
+        return json.loads(rv.data)
+
+    def get_template(self, template_id):
+        rv = self.app.get('/ctemplate/' + str(template_id),
+                            follow_redirects=True)
+        if rv.status_code != 200:
+            print 'delete template status_code error' + rv.status_code
+            return None
+        return json.loads(rv.data)
+
+    def get_templates(self):
+        rv = self.app.get('/ctemplate/' + str(template_id),
+                            follow_redirects=True)
+        if rv.status_code != 200:
+            print 'delete template status_code error' + rv.status_code
+            return None
+        return json.loads(rv.data)
 
     def get_template_types(self, parent_type_id=None):
         return self.app.get('/ctemplate/template_types', query_string=dict(
             parent_type_id = parent_type_id
         ), follow_redirects=True)
 
+    ###### Test Begin
     def template_type(self):
         # Get level 0 types
         rv = self.get_template_types()
         assert 'true' in rv.data
         # Get level 1 types
-        rv = self.get_template_types(4)
+        rv = self.get_template_types(1)
         assert 'true' in rv.data
 
-    def test_template_add(self):
+    def template_add_and_del(self):
         # Get level 0 types
-        rv = self.add_template()
-        print rv.json
+        add_result = self.add_template()
+        if add_result is None:
+            raise AssertionError
+        self.assertEqual(add_result.get('success'), True)
+        del_result = self.del_template(add_result.get('data'))
+        if del_result is None:
+            raise AssertionError
+        self.assertEqual(del_result.get('success'), True)
 
+    def template_update(self):
+        # Get level 0 types
+        add_result = self.add_template()
+        if add_result is None:
+            raise AssertionError
+
+        update_result = self.update_template(add_result.get('data'))
+        self.assertEqual(update_result.get('success'), True)
+
+        del_result = self.del_template(add_result.get('data'))
+        if del_result is None:
+            raise AssertionError
+
+    def template_get_one(self):
+        add_result = self.add_template()
+        if add_result is None:
+            raise AssertionError
+
+        get_result = self.get_template(add_result.get('data'))
+        if get_result is None:
+            raise AssertionError
+        self.assertEqual(get_result.get('success'), True)
+
+        del_result = self.del_template(add_result.get('data'))
+        if del_result is None:
+            raise AssertionError
+
+    def test_template_get_list(self):
+        add_result = self.add_template()
+        if add_result is None:
+            raise AssertionError
+
+        get_result = self.get_templates(add_result.get('data'))
+        if get_result is None:
+            raise AssertionError
+        self.assertEqual(get_result.get('success'), True)
+
+        del_result = self.del_template(add_result.get('data'))
+        if del_result is None:
+            raise AssertionError
 
 if __name__ == '__main__':
     unittest.main()
