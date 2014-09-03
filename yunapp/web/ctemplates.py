d5dccfb36c3ba01609e0c645e4726c4b6f7603ed
wenwu <xudabin@yunhetong.com>
2014-09-03 14:51:37 +0800

    add template

--- yunapp/web/ctemplates.py
+++ yunapp/web/ctemplates.py
@@ -4,6 +4,10 @@ from flask import Blueprint, render_template, request
 web_templates = Blueprint('web_templates', __name__)
 
 
-@web_templates.route('/template_detail.html', methods=['GET'])
+@web_templates.route('/template_detail', methods=['GET'])
 def template_detail():
     return render_template('contract_temp/template_detail.html')
+
+@web_templates.route('/template_list', methods=['GET'])
+def template_detail():
+    return render_template('contract_temp/template_list.html')
