fa7cfbeb933bd31b2995da2fe68ca790952558e0
seanwu <seanwu@yunhetong.net>
2014-09-04 14:10:40 +0800

    fix20

--- yunapp/contract_temp/constants.py
+++ yunapp/contract_temp/constants.py
@@ -4,4 +4,4 @@ ERROR_CODE = {
     'NO_SUCH_TEMPLATE_TYPE': 'No such template type, please check the type id param.',
     'NO_SUCH_TEMPLATE': 'No such template, pls check the id param.'
 }
-PAGE_SIZE = 5
+PAGE_SIZE = 20
