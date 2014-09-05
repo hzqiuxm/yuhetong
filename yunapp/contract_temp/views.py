0d7f8e084735138033a34777dbd85a54b13e05f9
seanwu <seanwu@yunhetong.net>
2014-09-05 15:30:03 +0800

    nothing

--- yunapp/contract_temp/views.py
+++ yunapp/contract_temp/views.py
@@ -189,9 +189,9 @@ def get_templates():
 
     filter_dict = dict(status=1)
     if 'template_type_id' in request.values:
-    t_type_id = request.values.get('template_type_id')
-    if t_type_id.isdigital() and int(t_type_id) > 0:
-        filter_dict['type_id'] = request.values.get('template_type_id')
+        t_type_id = request.values.get('template_type_id')
+        if t_type_id.isdigital() and int(t_type_id) > 0:
+            filter_dict['type_id'] = request.values.get('template_type_id')
     search_key = None
     if 'search_key' in request.values:
         search_key = request.values.get('search_key', '')
